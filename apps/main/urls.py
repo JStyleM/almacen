d42d35427ed9fdb82faa51c466bc0855ef5c1040
Jhonatan Moncada <jhonatanmoncada@gmail.com>
2017-12-19 15:16:55 -0500

    Intentando hacer el retiro

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+	url(r'^$', views.home, name='home'),
+    url(r'^(?P<item_id>\d+)/(?P<monto>\d+)$', views.item_update, name='retiro'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -5,7 +5,14 @@ from .models import Item
 # Create your views here.
 
 def home(request):
-
 	query = Item.objects.all()
 	return render(request, 'index.html', {'query':query})
 
+def item_update(request, item_id, monto):
+	print(item_id)
+	print(monto)
+	item = Item.objects.get(id=item_id)
+	item.cantidad = item.cantidad - int(monto)
+	item.save()
+	query = Item.objects.all()
+	return render(request, 'index.html', {'query':query})	

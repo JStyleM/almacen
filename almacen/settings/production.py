e40b1257f3b0771229c38fc47ce4e44fd9a03498
Jhonatan Moncada <jhonatanmoncada@gmail.com>
2017-12-26 15:57:56 -0500

    Intentando el Deploy

--- almacen/settings/production.py
+++ almacen/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -18,4 +19,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
